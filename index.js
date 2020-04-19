fd7b7b554b9bfa27f20d43d1abccf3bd40fb50b6
Muhtadi1Laskar <40865352+Muhtadi1Laskar@users.noreply.github.com>
2020-04-19 22:20:50 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,67 +1,69 @@
-const button = document.getElementById("btn");
-const tipOutput = document.getElementById("tips");
-const totalOutput = document.getElementById("total");
-const output = document.getElementById("output");
+let button = document.getElementById("btn");
+let tipOutput = document.getElementById("tips");
+let totalOutput = document.getElementById("total");
+let output = document.getElementById("output");
 
 const calculate_tip = () => {
-    const bill = Number(document.getElementById("bill").value);
-    const tipPercent = Number(document.getElementById("tip").value);
-    const totalPeople = Number(document.getElementById("number").value);
-    output.style.display = "block";
+  let bill = Number(document.getElementById("bill").value);
+  let tipPercent = Number(document.getElementById("tip").value);
+  let totalPeople = Number(document.getElementById("number").value);
+  output.style.display = "block";
 
+  if (bill == " " && tipPercent == " " && totalPeople == " ") {
+    let tipOutput = 0;
+    let totalOutput = 0;
+    document.getElementById("per-tip").innerText = "Tip per person: ";
+    tipOutput.innerText = totalTipSplit.toFixed(2);
+    document.getElementById("per-total").innerText = "Bill per person: ";
+    totalOutput.innerText = totalBillSplit.toFixed(2);
+  }
 
-    if(bill == " " &&  tipPercent == " " && totalPeople == " ") {
-        
-        tipOutput.innerText = "0";
-        totalOutput.innerText = "0";
-    }
+  if (tipPercent == 0) {
+      if(totalPeople == 0) {
+          totalPeople = 1;
+      }
+    let tip = 0;
 
-    if (tipPercent == 0) {
-        const tip = 0;
+    let totalTip = bill * tip;
+    let totalTipSplit = bill * tip;
+    let totalBill = bill + totalTip;
+    console.log(totalPeople);
+    let totalBillSplit = totalBill / totalPeople;
 
-        const totalTip = (bill * tip);
-        const totalTipSplit = (bill * tip);
-        const totalBill = bill + totalTip;
-        const totalBillSplit = totalBill / totalPeople ;
+    // Display only the total bill split as the tip is 0
+    document.getElementById("per-tip").style.display = "none";
+    tipOutput.style.display = "none";
+    totalOutput.innerText = totalBillSplit.toFixed(2);
+  } else if (totalPeople == 0 || totalPeople == " " || totalPeople == 1) {
+    // Convert percentage to decimal
+    let tip = tipPercent / 100;
+    let totalPeople = 1;
+    let totalTip = bill * tip;
+    let totalTipSplit = totalTip / totalPeople;
+    let totalBill = bill + totalTip;
+    let totalBillSplit = totalBill / totalPeople;
 
-        // Display only the total bill split as the tip is 0
-        document.getElementById("per-tip").style.display = "none";
-        tipOutput.style.display = "none"
-        totalOutput.innerText = totalBillSplit.toFixed(2);
+    // Display only "Total Tips" and "Total Bill" as only one is paying both
+    document.getElementById("per-tip").innerText = "Total Tip: ";
+    tipOutput.innerText = totalTipSplit.toFixed(2);
+    document.getElementById("per-total").innerText = "Total bill: ";
+    totalOutput.innerText = totalBillSplit.toFixed(2);
+  } else {
+    // Convert percentage to decimal
 
-    }
-    else if(totalPeople == 0 || totalPeople == " " || totalPeople == 1) {
-        // Convert percentage to decimal 
-        const tip = tipPercent / 100;
-        const totalPeople = 1;
-        const totalTip = (bill * tip);
-        const totalTipSplit = totalTip / totalPeople;
-        const totalBill = bill + totalTip;
-        const totalBillSplit = totalBill / totalPeople;
+    let tip = tipPercent / 100;
 
-        // Display only "Total Tips" and "Total Bill" as only one is paying both
-        document.getElementById("per-tip").innerText = "Total Tip: "
-        tipOutput.innerText = totalTipSplit.toFixed(2);
-        document.getElementById("per-total").innerText = "Total bill: "
-        totalOutput.innerText = totalBillSplit.toFixed(2);
-    }
-    else {
-        // Convert percentage to decimal 
-        const tip = tipPercent / 100;
+    let totalTip = bill * tip;
+    let totalTipSplit = totalTip / totalPeople;
+    let totalBill = bill + totalTip;
+    let totalBillSplit = totalBill / totalPeople;
 
-        const totalTip = (bill * tip);
-        const totalTipSplit = totalTip / totalPeople;
-        const totalBill = bill + totalTip;
-        const totalBillSplit = totalBill / totalPeople;
-
-        tipOutput.innerText = totalTipSplit.toFixed(2);
-        totalOutput.innerText = totalBillSplit.toFixed(2);
-    }
-
-    
-
-}
-
+    document.getElementById("per-tip").innerText = "Tip per person: ";
+    tipOutput.innerText = totalTipSplit.toFixed(2);
+    document.getElementById("per-total").innerText = "Bill per person: ";
+    totalOutput.innerText = totalBillSplit.toFixed(2);
+  }
+};
 
 // Event Listener
 button.addEventListener("click", calculate_tip);
